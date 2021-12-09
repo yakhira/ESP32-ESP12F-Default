5f059d3669c651fe7aded4a0b8648ad721f94ab1
yakhira <ruslan.k.yakhin@gmail.com>
2021-12-09 23:48:35 +0100

    First init

---
+++ README.md
@@ -0,0 +1,10 @@
+## WIFI setup
+
+SSID: ESP-<random>
+PASSWORD: ESPp@$$w0rd!
+IP: 192.168.4.1
+PORT: 8080
+
+## Reset
+
+Connect GPIO0 to GND
---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include "setup/wifi.h"
+
+// -------- DEFAULT SKETCH PARAMETERS --------
+const int SKETCH_VERSION = 1; 
+
+ESPWiFi espwifi("ESP32-D0WDQ5");
+
+void setup() {
+	espwifi.wifiConnect();
+	espwifi.updateSketch(SKETCH_VERSION);
+}
+
+void loop() {
+	if (WiFi.getMode() == WIFI_STA) {
+		if (WiFi.status() != WL_CONNECTED) {
+			WiFi.reconnect();
+		}
+	}
+
+	espwifi.stateCheck();
+}
