33e214dcc901fab092e546b181ca16cb283aa246
yakhira <ruslan.k.yakhin@gmail.com>
2022-07-19 17:59:08 +0200

    Remove isWebServerRunning

--- src/setup/wifi.cpp
+++ src/setup/wifi.cpp
@@ -14,7 +14,6 @@ const String ESPWiFi::configFile = "/wifi_config.json";
 AsyncWebServer server(80);
 
 ESPWiFi::ESPWiFi(String chipName){
-    isWebServerRunning = false;
     espChipName = chipName;
     resetCount = 0;
 }
@@ -122,8 +121,6 @@ void ESPWiFi::wifiConnect(){
 		});
 
         server.begin();
-
-        isWebServerRunning = true;
     }
 }
 
